a232bccd948996057b7ee15b9c51d60164450468
Zachary Hamilton <zacharyhamilton@Zacharys-MacBook-Air.local>
2023-09-14 00:16:25 -0400

    Added name to App

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,8 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    temperature = (temperature - 32) * (5 / 9);
+    return temperature;
 }
 
 /**
@@ -12,7 +13,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let total = 0;
+    if (first >= 0) {
+        total = total + first;
+    }
+    if (second >= 0) {
+        total = total + second;
+    }
+    if (third >= 0) {
+        total = total + third;
+    }
+    return total;
 }
 
 /**
@@ -20,7 +31,9 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    message = message.toUpperCase();
+    const returnMessage = `${message}!`;
+    return returnMessage;
 }
 
 /**
@@ -28,7 +41,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -37,5 +50,18 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes") {
+        return true;
+    }
+    if (word === "YES") {
+        return true;
+    }
+    if (word === "no") {
+        return false;
+    }
+    if (word === "NO") {
+        return false;
+    }
+
+    return null;
 }
