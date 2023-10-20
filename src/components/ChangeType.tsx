df00d385db699882f4c5c628be89045d9b6b174c
Zachary-E-Hamilton <zachary.hamilton@maine.edu>
2023-10-19 20:01:52 -0400

    c

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,25 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [currentType, setCurrentType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    const handleChange = () => {
+        if (currentType === "short_answer_question") {
+            setCurrentType("multiple_choice_question");
+        } else {
+            setCurrentType("short_answer_question");
+        }
+    };
+    return (
+        <div>
+            <Button onClick={handleChange}>Change Type</Button>
+
+            {currentType === "short_answer_question" && <div>Short Answer</div>}
+            {currentType === "multiple_choice_question" && (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
